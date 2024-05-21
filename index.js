0b8d83b1e19b91a7442c11459123484d5303a665
Aleksandar Stojkov <a.stojkov5@gmail.com>
2024-05-21 15:37:40 +0200

    Exercise 6

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let votingAge= 18;
-
-if (votingAge >= 18 )
+let score = 65;
+if (score >= 65 )
     {
-        console.log ("YOu can vote");
+        console.log ("Pass");
     }
-    else
-    {
-    console.log ("You can Vote");
-    } 
-let vote = votingAge>=18 ? "You can vote" : "Can't Vote"
-console.log (vote)  
+   else 
+   {
+    console.log ("Fail");
+   } 
+
+let result = score >=65 ? "Pass" : "Fail" ;
+console.log (result);
+
