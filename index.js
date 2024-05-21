78ea33e7a2282ea73870d235e0b5137b6ef220c7
Aleksandar Stojkov <a.stojkov5@gmail.com>
2024-05-21 15:06:37 +0200

    Exercise 2

---
+++ index.js
@@ -0,0 +1,19 @@
+let dayOffWeek = "Tuesday";
+
+if (dayOffWeek === "Monday"){
+console.log ("Start of the work week!");
+}
+else if (dayOffWeek === "Wednesday")
+    {
+        console.log ("Midweek!");
+
+    }
+else if (dayOffWeek === "Friday")
+    {
+        console.log ("Almost weekend!");
+
+    }   
+    else 
+    {
+        console.log ("Just another day");
+    } 
