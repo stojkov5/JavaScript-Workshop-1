5df41760f729e188b6c4400a5ad1a485f9009af4
Aleksandar Stojkov <a.stojkov5@gmail.com>
2024-05-21 14:33:53 +0200

    Exercise 1 commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let age = 10;
+
+if (age < 12) {
+    console.log ("Child Ticket:$5");
+}
+else if (age >=12 && age < 18)
+    {
+        console.log ("Teen ticket:$7");
+    }
+    else {
+        console.log("Senior Ticket");
+    }
