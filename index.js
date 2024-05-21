d9b348aa1d4496fa0bd1ef5f004960636dfd7a24
Aleksandar Stojkov <a.stojkov5@gmail.com>
2024-05-21 15:31:08 +0200

    Exercise 5

---
+++ index.js
@@ -0,0 +1,12 @@
+let votingAge= 18;
+
+if (votingAge >= 18 )
+    {
+        console.log ("YOu can vote");
+    }
+    else
+    {
+    console.log ("You can Vote");
+    } 
+let vote = votingAge>=18 ? "You can vote" : "Can't Vote"
+console.log (vote)  
